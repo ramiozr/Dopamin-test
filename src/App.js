939baf66dc3b703fa1976d3ac3f5c2320106aa77
ramiozr <150791145+ramiozr@users.noreply.github.com>
2026-02-07 15:14:36 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DopamineSurvey from './dopamine_survey.jsx'; // Importe ton questionnaire
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DopamineSurvey />
+    </div>
+  );
+}
+
+export default App;
